9924251fc5ea5ce15258b5605379c98a1aaec537
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-04-26 08:11:42 +1200

    initial commit for learning ruby

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+	def index
+		@articles = Article.all
+	end
+  
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = 'Article was sucessfully created'
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+		# @article = Article.new(article_params)
+		# @article.save
+		# redirect_to article_show(@article)
+	end
+
+	def edit
+		# @article = Article.find(params[:id])
+	end
+
+	def update 
+		# @article = Article id(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = 'Article was sucessfully updated'
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was sucessfully deleted"
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+	private 
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
+
+
+end
